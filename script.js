0a048bd1d8b3cdefa632e100da08073664e7f305
iptc-01 <estagioensino02@iptc.net.br>
2026-01-02 11:19:00 -0300

    new endpoint, changed backend

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', function () {
   const reservas = {};
   const feriados = ['2026-01-01', '2026-12-25'];
   const senhaAdmin = 'SecretariaIPTC2026';
-  const ENDPOINT = "https://script.google.com/macros/s/AKfycbyptEXGc9JBLtbXXC6W75-OB3Y0fZ84o3i5Muy-ejVShHqQiPLi_Te1JGNRr-79x9ue/exec";
+  const ENDPOINT = "https://script.google.com/macros/s/AKfycbxK4rsj9EZpv7jFE07RvaecNglIXRbfPlJlMihxVBYwZDnyyNiGGuv9kzp1WgKNGDJV/exec";
 
   let usuarioAdmin = false;
   let dataSelecionada = null;
